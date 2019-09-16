6b01b4851a207467b5641323ffd9781173ac92e1
Leuk Travis Mangila <travismangila1999@gmail.com>
2019-09-16 20:17:44 +1000

    Created: MasterPageViewModel

---
+++ DungeonTasker/DungeonTasker/ViewModel/MasterPageViewModel.cs
@@ -0,0 +1,121 @@
+﻿using DungeonTasker.Models;
+using DungeonTasker.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DungeonTasker.ViewModel
+{
+    public class MasterPageViewModel
+    {
+        public Command Tasks_Clicked { get; set; }
+        public Command Inventory_Clicked { get; set; }
+        public Command Dungeon_Clicked { get; set; }
+        public Command Settings_Clicked { get; set; }
+
+        Page page;// This is the tasks page set to be binded to the Tasks_clicked command
+        User user;
+        InventoryItems items;
+        Dungeon dungeon;
+        WeaponInfo weapon;
+        Stats stats;
+        logged truth;
+        MasterPage mainpage;
+        ContentPage display;
+
+        /*
+         * Contructor for Masterpage, initialize all components and BindingContext 
+         * 
+         * PARAM
+         * page: encapsulate page
+         * user: encapsulate user
+         * items: encapsulate items
+         * weapon: encapsulate weapon
+         * truth: encapsulate truth
+         * mainpage: encapsulate mainpage
+         * ContentPage: encapsulate display
+         * 
+         * RETURNS Nothing
+         */
+        public MasterPageViewModel(Page page, User user, InventoryItems items, WeaponInfo weapon, Stats stats, logged truth, MasterPage mainpage, ContentPage display)
+        {
+            this.page = page;
+            this.user = user;
+            this.items = items;
+            this.truth = truth;
+            this.weapon = weapon;
+            this.stats = stats;
+            this.mainpage = mainpage;
+            this.display = display;
+            weapon.SetWeapon(display, User.CheckForstring(items.Invfile, "Equipped:"));
+            dungeon = new Dungeon(this.user, this.items, this.weapon, this.stats);
+            Tasks_Clicked = new Command(() => TaskNav());
+            Inventory_Clicked = new Command(() => InventoryNav());
+            Dungeon_Clicked = new Command(() => DungeonNav());
+            Settings_Clicked = new Command(() => SettingsNav());
+        }
+
+        /*
+         * If this is selected change the detailspage to the taskpage
+         * 
+         * PARAM 
+         * sender: reference to the control object
+         * eventargs: object data
+         * RETURNS Nothing
+         */
+        private void TaskNav()
+        {
+            ((MasterDetailPage)mainpage.Parent).Detail = page;
+            ((MasterDetailPage)mainpage.Parent).IsPresented = false;   
+        }
+
+        /*
+         * If this is selected change the detailspage to the storepage
+         * 
+         * PARAM 
+         * sender: reference to the control object
+         * eventargs: object data
+         * RETURNS Nothing
+         */
+        private void InventoryNav()
+        {
+
+            ((MasterDetailPage)mainpage.Parent).Detail = new NavigationPage(new Inventory(items, this.weapon));
+            ((MasterDetailPage)mainpage.Parent).IsPresented = false;
+
+        }
+
+        /*
+        * If this is selected change the detailspage to the Dungeonpage
+        * 
+        * PARAM 
+        * sender: reference to the control object
+        * eventargs: object data
+        * RETURNS Nothing
+        */
+        private void DungeonNav()
+        {
+            dungeon.selectKey();
+            ((MasterDetailPage)mainpage.Parent).Detail = new NavigationPage(dungeon);
+            ((MasterDetailPage)mainpage.Parent).IsPresented = false;
+
+        }
+
+        /*
+         * If this is selected change the detailspage to the settings
+         * 
+         * PARAM 
+         * sender:reference to the control object
+         * eventargs: object data
+         * RETURNS Nothing
+         */
+        private void SettingsNav()
+        {
+
+            ((MasterDetailPage)mainpage.Parent).Detail = new NavigationPage(new Settings(user, truth));
+            ((MasterDetailPage)mainpage.Parent).IsPresented = false;
+        }
+    }
+}
--- DungeonTasker/DungeonTasker/Views/MasterPage.xaml.cs
+++ DungeonTasker/DungeonTasker/Views/MasterPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using DungeonTasker.Models;
+using DungeonTasker.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -15,21 +16,14 @@ namespace DungeonTasker.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class MasterPage : ContentPage
 	{
-        Page page;
-        User user;
-        InventoryItems items;
-        Dungeon dungeon;
-        WeaponInfo weapon;
-        Stats stats;
-        logged truth;
-
         /*
-         * Contructor for Masterpage, initialize all variables 
+         * Contructor for Masterpage, initialize all components and BindingContext 
          * 
          * PARAM
          * page: encapsulate page
          * user: encapsulate user
          * items: encapsulate items
+         * weapon: encapsulate weapon
          * truth: encapsulate truth
          * 
          * RETURNS Nothing
@@ -38,80 +32,7 @@ namespace DungeonTasker.Views
         public MasterPage (Page page, User user, InventoryItems items, WeaponInfo weapon, Stats stats,  logged truth)
 		{
 			InitializeComponent ();
-            this.page = page;
-            this.user = user;
-            this.items = items;
-            this.truth = truth;
-            this.weapon = weapon;
-            this.stats = stats;
-            weapon.SetWeapon(this, User.CheckForstring(items.Invfile, "Equipped:"));
-            dungeon = new Dungeon(this.user, this.items, this.weapon, this.stats);
-		}
-      
-
-        /*
-         * If this is selected change the detailspage to the taskpage
-         * 
-         * PARAM 
-         * sender: reference to the control object
-         * eventargs: object data
-         * RETURNS Nothing
-         */
-
-        private void Tasks_Clicked(object sender, EventArgs e)
-        {
-            ((MasterDetailPage)Parent).Detail = page;
-            ((MasterDetailPage)Parent).IsPresented = false;
-        }
-
-        /*
-         * If this is selected change the detailspage to the storepage
-         * 
-         * PARAM 
-         * sender: reference to the control object
-         * eventargs: object data
-         * RETURNS Nothing
-         */
-        private void Inventory_Clicked(object sender, EventArgs e)
-        {
-            
-            ((MasterDetailPage)Parent).Detail = new NavigationPage(new Inventory(items, this.weapon));
-            ((MasterDetailPage)Parent).IsPresented = false;
-
-        }
-
-        /*
-         * If this is selected change the detailspage to the Dungeonpage
-         * 
-         * PARAM 
-         * sender: reference to the control object
-         * eventargs: object data
-         * RETURNS Nothing
-         */
-
-        private void Dungeon_Clicked(object sender, EventArgs e)
-        {
-            dungeon.selectKey();
-            ((MasterDetailPage)Parent).Detail = new NavigationPage(dungeon);
-            ((MasterDetailPage)Parent).IsPresented = false;
-
-        }
-
-
-        /*
-         * If this is selected change the detailspage to the settings
-         * 
-         * PARAM 
-         * sender:reference to the control object
-         * eventargs: object data
-         * RETURNS Nothing
-         */
-
-        private void Settings_Clicked(object sender, EventArgs e)
-        {
-            
-            ((MasterDetailPage)Parent).Detail = new NavigationPage(new Settings(user, truth));
-            ((MasterDetailPage)Parent).IsPresented = false;
+            BindingContext = new MasterPageViewModel(page,user,items,weapon,stats,truth,this,this);
         }
     }
 }
