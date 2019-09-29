675a5003ba71c5ee29b4efc1834a0242e734cc8b
Simon Han <n9977741@qut.edu.au>
2019-09-29 23:47:11 +1000

    About Us page completed (Button + all content) Stats button added (No content/ functionality for stats page)

--- DungeonTasker/DungeonTasker/Views/AboutUsView.xaml.cs
+++ DungeonTasker/DungeonTasker/Views/AboutUsView.xaml.cs
@@ -9,43 +9,13 @@ namespace DungeonTasker
 {
     public partial class AboutUsView : ContentPage
     {
-        String[] SS = new string[] { "This", "Is", "A", "Test" };
-        int i = 1;
+
 
         public AboutUsView()
         {
             InitializeComponent();
-            NewSlideBoi.Value = 1;
             
 
-        }
-
-        private void Button_Clicked(object sender, EventArgs e)
-        {
-            try
-            {
-                Quotes.Text = SS[i];
-            }catch(IndexOutOfRangeException){
-                i = 0;
-                Quotes.Text = SS[i];
-            }
-                i++;
-        }
-
-        private void NewSlideBoi_ValueChanged(object sender, ValueChangedEventArgs e)
-        {
-            double nice;
-            nice = NewSlideBoi.Value * 100;
-            i = Convert.ToInt32(nice)/4;
-
-            if (i == 0)
-            {
-                i = 1;
-            }
-
-            FontSize.Text = "FontSize = " + i.ToString();
-
-            Quotes.FontSize = i;
         }
     }
 }
